c212d2b9b89b3e0376920ba3ef323686f5f05f0b
Xanonymous <trusaidlin@gmail.com>
2024-04-28 18:09:22 +0100

    test: 💍 second version

--- code.java
+++ code.java
@@ -1,10 +1,14 @@
     void printOwning() {
         // Print banner
         System.out.println("Details of account");
-        System.out.println("---");
+        System.out.println("***");
         System.out.println("");
 
         // Print details.
+        printDetails();
+    }
+
+    void printDetails() {
         System.out.println("name:" + name);
         System.out.println("amount:" + outstanding);
     }
